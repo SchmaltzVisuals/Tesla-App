9ad843d59cf39aa2779db455640e08451b7be800
Schmaltz <SiriusVisualsBusiness@gmail.com>
2022-08-03 14:17:49 -0400

    Car Item Component

--- my-app/App.js
+++ my-app/App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from "expo-status-bar"
-import { StyleSheet, View } from "react-native"
-import CarItem from "./components/CarItem"
+import { StyleSheet, Text, View } from "react-native"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem />
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   )
