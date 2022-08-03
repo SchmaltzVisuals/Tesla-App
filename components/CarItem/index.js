eb1578dff638174059339ca571dbbf55fe8e3ac9
Schmaltz <SiriusVisualsBusiness@gmail.com>
2022-08-03 18:57:24 -0400

    Car Item Component

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -3,31 +3,34 @@ import styles from "./styles"
 import StyledButton from "../StyledButton"
 
 const CarItem = (props) => {
+  const { name, tagline, image, taglineCTA } = props
   return (
     <View style={styles.carContainer}>
-      <ImageBackground
-        source={require("../../assets/ModelX.jpeg")}
-        style={styles.image}
-      />
+      <ImageBackground source={image} style={styles.image} />
       <View style={styles.titles}>
-        <Text style={styles.title}>Model S</Text>
-        <Text style={styles.subtitle}>Starting at $67,045</Text>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subtitle}>
+          {tagline}{" "}
+          <Text style={styles.subtitleCTA}>{taglineCTA}</Text>
+        </Text>
       </View>
 
-      <StyledButton
-        type="primary"
-        content={"Customer Order"}
-        onPress={() => {
-          console.warn("custom order pressed")
-        }}
-      />
-      <StyledButton
-        type="secondary"
-        content={"Existing Inventory"}
-        onPress={() => {
-          console.warn("Existing Inventory Pressed")
-        }}
-      />
+      <View style={styles.buttonsContainer}>
+        <StyledButton
+          type="primary"
+          content={"Customer Order"}
+          onPress={() => {
+            console.warn("custom order pressed")
+          }}
+        />
+        <StyledButton
+          type="secondary"
+          content={"Existing Inventory"}
+          onPress={() => {
+            console.warn("Existing Inventory Pressed")
+          }}
+        />
+      </View>
     </View>
   )
 }
